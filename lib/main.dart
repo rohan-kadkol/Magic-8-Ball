642a2436c2fc5edb5375c39a36e0091683a8de59
Rohan Kadkol <rohan-kadkol@users.noreply.github.com>
2020-06-27 13:56:03 -0500

    Completed App

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,42 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-      MaterialApp(
-        home: null,
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int ballNumber = getRandomBallNumber();
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Magic 8 Ball'),
+        ),
+        backgroundColor: Colors.blue[100],
+        body: Center(
+          child: FlatButton(
+            child: Image.asset('images/ball$ballNumber.png'),
+            onPressed: updateBallNumber,
+          ),
+        ),
       ),
     );
+  }
+
+  void updateBallNumber() {
+    setState(() {
+      ballNumber = getRandomBallNumber();
+    });
+  }
+
+  static int getRandomBallNumber() {
+    return Random().nextInt(5) + 1;
+  }
+}
